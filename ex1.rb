70db9b8f8dc4b1e57cf70e546bbec594878c172c
Jay <jayamko@hotmail.com>
2021-03-31 03:58:21 -0600

    new

--- ex1.rb
+++ ex1.rb
@@ -3,6 +3,6 @@
 3.141528                // float literal
 true                    // boolean literal
 { 'a' => 1, 'b' => 2 }  // hash literal
-[ 1, 2, 3 ]             //array literal
+[ 1, 2, 3 ]             // array literal
 :sym                    // symbol literal
 nil                     // nil literal
